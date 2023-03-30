a8089bb1bd070cd8f3653b5f23ee65e03667234b
Yehia-Gamal <yahia.elspaa@gmail.com>
2023-03-30 10:36:06 +0200

    The Projects and Services section has been added and its images uploaded

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Start Statistic 
 let counters = document.querySelectorAll(".number");
 
 counters.forEach((counter) => {
@@ -20,3 +21,32 @@ counters.forEach((counter) => {
   };
   updateCounter();
 });
+// End Statistic 
+
+// Start Projects
+let switcherList = document.querySelectorAll(".switcher li");
+let imgs = document.querySelectorAll(".gallery .all");
+
+switcherList.forEach((li) => {
+  li.addEventListener("click", removeActive);
+  li.addEventListener("click", manageImgs);
+});
+
+// Remove Active Class From All Lis And Add Class Active Only To Current
+function removeActive() {
+  switcherList.forEach((li) => {
+    li.classList.remove("active");
+    this.classList.add("active", "rounded-pill");
+  });
+}
+
+// Manage Imgs
+function manageImgs() {
+  imgs.forEach((img) => {
+    img.style.display = "none";
+  });
+  document.querySelectorAll(this.dataset.cat).forEach((el) => {
+    el.style.display = "block"
+  })
+}
+// End Projects
